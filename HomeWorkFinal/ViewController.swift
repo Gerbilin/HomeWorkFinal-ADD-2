9f1c2dbf264000d9be02ae0160985ac4c60db50b
Gerbilin <gerbilin@yandex.ru>
2022-05-02 06:09:49 +0300

    Delete all comments.

--- HomeWorkFinal/ViewController.swift
+++ HomeWorkFinal/ViewController.swift
@@ -1,9 +1,3 @@
-//
-//  ViewController.swift
-//  HomeWorkFinal
-//
-//  Created by Gerbilin Mac on 02.05.2022.
-//
 
 import UIKit
 
@@ -11,7 +5,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
